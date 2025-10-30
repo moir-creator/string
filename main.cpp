eae10033df1c26bb7e9846bce0fa27cc7ce17627
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-10-30 20:37:27 +0300

    method join

--- main.cpp
+++ main.cpp
@@ -25,6 +25,39 @@ int increase(char*& array, int& size) {
     return sizeIncrease;
 }
 
+void join(char*& strFirst, char*& strSecond, int& sizeFirst, int sizeSecond) {
+    //checking incoming data
+    if (strSecond == nullptr) {
+        std::cout << "Error! " << strSecond << " is NULL." << std::endl;
+        return;
+    }
+    if (strFirst == nullptr && strSecond == nullptr) {
+        std::cout << "Error! The arrays are NULL!";
+        return;
+    }
+    if (strFirst == nullptr) {
+        strFirst = strSecond;
+        return;
+    }
+    //count number of contains chars
+    int lengthFirst = length(strFirst, sizeFirst);
+    int lengthSecond = length(strSecond, sizeSecond);
+    //chek the need to add new array
+    if ((lengthFirst + lengthSecond) > sizeFirst-1) {
+        //increase(strFirst, sizeFirst);
+        sizeFirst = increase(strFirst, sizeFirst); /* or we can use recursion instead
+        join(strFirst, strSecond, sizeFirst, sizeSecond); */
+    }
+    //define the end of arrFirst string
+    int i = lengthFirst;
+    //now copy strSecond to the end of strFirst string
+    for (int j = 0; strSecond[j] != '\0' && j < sizeSecond; j++) {
+        strFirst[i] = strSecond[j];
+        i++;
+    }
+    strFirst[i] = '\0';
+}
+
 void output(char arr[], int size) {
     int i = 0;
     while (arr[i] != '\0' && i < size) {
@@ -35,11 +68,17 @@ void output(char arr[], int size) {
 }
 
 void fullControl(char arr[], int size) {
+    std::cout << "CHECKING THE ARRAY: ";
     int i = 0;
     for ( ; i < size; i++) {
+        if (arr[i] == '\0') {
+            std::cout << "\\0 ";
+        }
         std::cout << "array{" << i << "] = " << arr[i] << " ";
     }
-    std::cout << "TOTAL: " << i << "cells";
+    //std::cout << std::endl;
+    std::cout << "TOTAL: " << i << "cells" << std::endl;
+    //std::cout << "\033[H\033[H";
 }
 int main() {
     int sizeFirst = 10;
@@ -53,8 +92,10 @@ int main() {
     //std::cout << length(arrSecond, sizeSecond);
     output(arrFirst, sizeFirst);
     output(arrSecond, sizeSecond);
-    std::cout << increase(arrFirst, sizeFirst) << std::endl;
-    std::cout << sizeFirst << std::endl;
+    join(arrFirst, arrSecond, sizeFirst, sizeSecond);
+    //std::cout << increase(arrFirst, sizeFirst) << std::endl;
+    //std::cout << sizeFirst << std::endl;
+    output(arrFirst, sizeFirst);
     fullControl(arrFirst, sizeFirst);
 
     return 0;
