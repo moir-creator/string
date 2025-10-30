f085b4eeabb6a2de11a171a4e8ac0a1a08eeb4b8
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-10-30 19:02:16 +0300

    int length

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+int lenght(char arr[], int size) {
+    int count = 0;
+    for (int i = 0; arr[i] != '\0' && i < size; i++) {
+        count++;
+    }
+    return count;
+}
+
+
 int main() {
 
     return 0;
