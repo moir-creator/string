b87b1a540ebf509eca204d21fa22a4d4fac28117
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-10-30 19:11:59 +0300

    output, initialization of dynamic arrays

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 
-int lenght(char arr[], int size) {
+int length(char arr[], int size) {
     int count = 0;
     for (int i = 0; arr[i] != '\0' && i < size; i++) {
         count++;
@@ -8,9 +10,26 @@ int lenght(char arr[], int size) {
     return count;
 }
 
-
+void output(char arr[], int size) {
+    int i = 0;
+    while (arr[i] != '\0' && i < size) {
+        std::cout << arr[i];
+        i++;
+    }
+    std::cout << std::endl;
+}
 int main() {
+    int sizeFirst = 10;
+    int sizeSecond = 10;
+    char* arrFirst = new char[sizeFirst];
+    char* arrSecond = new char[sizeSecond];
+    strcpy(arrFirst, "Visual ");
+    strcpy(arrSecond, "comfort");
 
+    //std::cout << length(arrFirst, sizeFirst);
+    //std::cout << length(arrSecond, sizeSecond);
+    output(arrFirst, sizeFirst);
+    output(arrSecond, sizeSecond);
     return 0;
 
 }
