0aa502371ec25ec1c1c044d3061d4f49d6a2dd9b
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-10-30 21:11:19 +0300

    defineIdentity of two strings

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int increase(char*& array, int& size) {
     return sizeIncrease;
 }
 
-void join(char*& strFirst, char*& strSecond, int& sizeFirst, int sizeSecond) {
+void joinStrings(char*& strFirst, char*& strSecond, int& sizeFirst, int sizeSecond) {
     //checking incoming data
     if (strSecond == nullptr) {
         std::cout << "Error! " << strSecond << " is NULL." << std::endl;
@@ -46,7 +46,7 @@ void join(char*& strFirst, char*& strSecond, int& sizeFirst, int sizeSecond) {
     if ((lengthFirst + lengthSecond) > sizeFirst-1) {
         //increase(strFirst, sizeFirst);
         sizeFirst = increase(strFirst, sizeFirst); /* or we can use recursion instead
-        join(strFirst, strSecond, sizeFirst, sizeSecond); */
+        joinStrings(strFirst, strSecond, sizeFirst, sizeSecond); */
     }
     //define the end of arrFirst string
     int i = lengthFirst;
@@ -56,6 +56,25 @@ void join(char*& strFirst, char*& strSecond, int& sizeFirst, int sizeSecond) {
         i++;
     }
     strFirst[i] = '\0';
+}
+
+bool defineIdentity(char strFirst[], char strSecond[], int sizeFirst) {
+    if (strFirst == nullptr || strSecond == nullptr) {
+        return false;
+    }
+    int i = 0;
+    for ( ; strFirst[i] != '\0' && strSecond[i] != '\0' && i < sizeFirst; ) {
+        if (strFirst[i] == strSecond[i]) {
+            i++;
+        }
+    }
+    if (strFirst[i] == strSecond[i]) {
+        std::cout << "The strings are identical!" << std::endl;
+        return true;
+    } else {
+        std::cout << "The strings are NOT identical!" << std::endl;
+        return false;
+    }
 }
 
 void output(char arr[], int size) {
@@ -85,18 +104,19 @@ int main() {
     int sizeSecond = 10;
     char* arrFirst = new char[sizeFirst];
     char* arrSecond = new char[sizeSecond];
-    strcpy(arrFirst, "Visual ");
-    strcpy(arrSecond, "comfort");
+    strcpy(arrFirst, "Visual");
+    strcpy(arrSecond, "Visual");
 
     //std::cout << length(arrFirst, sizeFirst);
     //std::cout << length(arrSecond, sizeSecond);
     output(arrFirst, sizeFirst);
     output(arrSecond, sizeSecond);
-    join(arrFirst, arrSecond, sizeFirst, sizeSecond);
+    defineIdentity(arrFirst, arrSecond, sizeFirst);
+    //joinStrings(arrFirst, arrSecond, sizeFirst, sizeSecond);
     //std::cout << increase(arrFirst, sizeFirst) << std::endl;
     //std::cout << sizeFirst << std::endl;
-    output(arrFirst, sizeFirst);
-    fullControl(arrFirst, sizeFirst);
+    //output(arrFirst, sizeFirst);
+    //fullControl(arrFirst, sizeFirst);
 
     return 0;
 
